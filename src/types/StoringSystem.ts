e7ac4edc93e7cdfe452f2d56e192f2753bb0497d
HunteRoi <me@tinaeldevresse.eu>
2022-11-15 22:58:13 +0100

    feat: prevent user to use same data to verify an account

--- src/types/StoringSystem.ts
+++ src/types/StoringSystem.ts
@@ -4,5 +4,6 @@ import { IUser } from '.';
 
 export interface IStoringSystem<TUser extends IUser> {
   read: (userid: Snowflake) => Promise<TUser>;
+  readBy: (callback: (user: TUser, index: number | string) => boolean) => Promise<TUser>;
   write: (user: TUser) => Promise<void>;
 }
